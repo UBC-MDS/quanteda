24afb8401fa69962ec1f2496426bb4642e3cb61d
Merete Lutz <meretelutz@gmail.com>
2024-01-12 11:46:23 -0800

    Initial docstring framework

---
+++ src/quanteda/plot_num_dist.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import altair as alt
+
+def plot_num_dist(data):
+    """
+    Create a chart of histograms for each numeric feature in a dataset.
+
+    Parameters
+    ----------
+    data : Data
+        An object containing the dataset of interest
+
+    Returns
+    -------
+    self: Chart object
+        Returns an altair chart object
+
+    Examples
+    --------
+    >>> plot_num_dist(dataset)
+
+    """
+    return
