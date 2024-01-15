88cf03a0325abb474d79addaf5484ebb9e814ec3
Merete Lutz <meretelutz@gmail.com>
2024-01-15 13:20:24 -0800

    First pass at function body

--- src/quanteda/plot_num_dist.py
+++ src/quanteda/plot_num_dist.py
@@ -20,6 +20,18 @@ def plot_num_dist(data):
     >>> plot_num_dist(dataset)
 
     """
+    numeric_features = data.select_dtypes(include=np.number).columns.tolist()
+
+    plot = alt.Chart(data).mark_bar().encode(
+        alt.X(alt.repeat()).type('quantitative').bin(maxbins=20),
+        alt.Y('count()', stack=None).title('Frequency of Occurrence'),
+    ).properties(
+        width = 125,
+        height = 125
+    ).repeat(
+        numeric_features,
+        columns = 3
+    )
     return
 
 
