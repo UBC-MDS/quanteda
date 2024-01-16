c1ff7e87c3db00927320a1fb87e4462b02d7c6be
Jake Barnabe <jake.barnabe@queensu.ca>
2024-01-15 20:37:53 -0800

    Trying to make the fixture request work

--- tests/test_plot_missing_vals.py
+++ tests/test_plot_missing_vals.py
@@ -3,18 +3,20 @@ import pandas as pd
 from quanteda.plot_missing_vals import plot_missing_vals
 import altair as alt
 
-test_plot = plot_missing_vals(input_daily_returns_with_nan_df)
-
 def test_plot_type ():
+    test_plot = plot_missing_vals(input_daily_returns_with_nan_df)
     assert type(test_plot) == altair.Chart
 
 def test_mark_type():
+    test_plot = plot_missing_vals(input_daily_returns_with_nan_df)
     assert test_plot.to_dict()['mark']['type'] == 'rect'
 
 def test_plot_encoding():
+    test_plot = plot_missing_vals(input_daily_returns_with_nan_df)
     assert test_plot.to_dict()['encoding']['color']['field'] == 'value'
 
 def test_plot_legend_title():
+    test_plot = plot_missing_vals(input_daily_returns_with_nan_df)
     assert test_plot.to_dict()['encoding']['color']['title'] == 'NaN'
 
 
