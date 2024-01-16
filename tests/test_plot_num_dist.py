97ef273fde2823f3ead2c945176a1f730cedbd6a
Merete Lutz <meretelutz@gmail.com>
2024-01-15 16:26:09 -0800

    tests/conftest.py

--- tests/test_plot_num_dist.py
+++ tests/test_plot_num_dist.py
@@ -1,23 +1,20 @@
 import pytest
 import altair as alt
 
-
 from quanteda.plot_num_dist import plot_num_dist
 
-test_data = input_minute_returns_df
-test_plot = plot_num_dist(test_data)
 
 def test_plot_type():
-    assert isinstance(test_plot, alt.RepeatChart), "The chart created is not an altair repeated chart"
+    assert isinstance(num_dist_plot, alt.RepeatChart), "The chart created is not an altair repeated chart"
 
 def test_plot_col_num():
-    assert test_plot.columns == 3, "The chart has the wrong number of columns"
+    assert num_dist_plot.columns == 3, "The chart has the wrong number of columns"
 
 def test_mark_type():
-    assert test_plot.to_dict()['spec']['mark']['type'] == 'bar', "The marks on the chart are not bars"
+    assert num_dist_plot.to_dict()['spec']['mark']['type'] == 'bar', "The marks on the chart are not bars"
 
 def test_feature_type():
-    assert test_plot.to_dict()['spec']['encoding']['x']['type'] == 'quantitative', "The variables being plotted are not numeric"
+    assert num_dist_plot.to_dict()['spec']['encoding']['x']['type'] == 'quantitative', "The variables being plotted are not numeric"
 
 def test_plot_aggregate():
-    assert test_plot.to_dict()['spec']['encoding']['y']['aggregate'] == 'count', "The y-axis does not represent a count"
+    assert num_dist_plot.to_dict()['spec']['encoding']['y']['aggregate'] == 'count', "The y-axis does not represent a count"
